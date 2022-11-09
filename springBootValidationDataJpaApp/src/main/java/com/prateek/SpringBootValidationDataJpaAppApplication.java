d53f25ef759699bdc5c4a591998dcbb0c1a729e3
PRATEEK CHAUHAN <105979795+PRA3EEK@users.noreply.github.com>
2022-11-09 17:13:54 +0530

    added

---
+++ springBootValidationDataJpaApp/src/main/java/com/prateek/SpringBootValidationDataJpaAppApplication.java
@@ -0,0 +1,13 @@
+package com.prateek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootValidationDataJpaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootValidationDataJpaAppApplication.class, args);
+	}
+
+}
